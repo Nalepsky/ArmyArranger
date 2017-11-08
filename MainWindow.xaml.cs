32a933cdab85b8fdf30704ff03756953968acea4
Ganlog <Piotr.E.Nowakowski@gmail.com>
2017-11-08 11:41:50 +0100

    Email test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace ArmyArranger
 {
@@ -25,5 +26,35 @@ namespace ArmyArranger
             InitializeComponent();
         }
 
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            String subject = "Wololo test";
+            String message = "Mail sample content";
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("armyarranger@gmail.com");
+                mail.To.Add("armyarranger@gmail.com");
+                mail.Subject = subject;
+                mail.Body = message;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("ArmyArranger", "ArmyArrangerPass");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                MessageBox.Show("Mail został pomyslnie wysłany. Dziękujemy za kontakt.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Wysyłanie nie powiodło się. Sprawdź swoje połączenie z internetem.");
+                //MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        
     }
 }
