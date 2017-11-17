48c1e3f5b56680540f53ceb9eddccc84089e7ad3
Nalepsky <nalepka.marek@student.pk.edu.pl>
2017-11-17 02:14:55 +0100

    Change menu layout, add new options

--- ViewModels/MenuViewModel.cs
+++ ViewModels/MenuViewModel.cs
@@ -12,29 +12,45 @@ namespace ArmyArranger.ViewModels
     public class MenuViewModel
     {
         #region Commands
-
-        public ICommand AddUnits { get; set; }
-        public ICommand ContactUs { get; set; }
+       
         public ICommand CreateArmyList { get; set; }
+        //public ICommand AddUnits { get; set; }
+        public ICommand EditYourArmies { get; set; }
+        public ICommand ContactUs { get; set; }
+        public ICommand About { get; set; }
+        public ICommand Exit { get; set; }
 
         #endregion
 
-        #region
+        #region Constructors
 
         public MenuViewModel()
         {
-            AddUnits = new DelegateCommand(ChangeViewToAddUnits);
-            ContactUs = new DelegateCommand(ChangeViewToContactUs);
+            //AddUnits = new DelegateCommand(ChangeViewToAddUnits);            
             CreateArmyList = new DelegateCommand(ChangeViewToCreateArmyList);
+            EditYourArmies = new DelegateCommand(ChangeViewToEditYourArmies);
+            ContactUs = new DelegateCommand(ChangeViewToContactUs);
+            About = new DelegateCommand(ChangeViewToAbout);
+            Exit = new DelegateCommand(ChangeViewToExit);
         }
 
         #endregion
 
         #region Actions
 
-        private void ChangeViewToAddUnits()
+        //private void ChangeViewToAddUnits()
+        //{
+        //    App.Current.MainWindow.DataContext = new AddUnitsViewModel();
+        //}
+
+        private void ChangeViewToCreateArmyList()
         {
-            App.Current.MainWindow.DataContext = new AddUnitsViewModel();
+            App.Current.MainWindow.DataContext = new CreateArmyListViewModel();
+        }
+
+        private void ChangeViewToEditYourArmies()
+        {
+            App.Current.MainWindow.DataContext = new EditYourArmiesViewModel();
         }
 
         private void ChangeViewToContactUs()
@@ -42,9 +58,14 @@ namespace ArmyArranger.ViewModels
             App.Current.MainWindow.DataContext = new ContactUsViewModel();
         }
 
-        private void ChangeViewToCreateArmyList()
+        private void ChangeViewToAbout()
         {
-            App.Current.MainWindow.DataContext = new CreateArmyListViewModel();
+            App.Current.MainWindow.DataContext = new AboutViewModel();
+        }
+
+        private void ChangeViewToExit()
+        {
+            //app shoud close here
         }
 
         #endregion
